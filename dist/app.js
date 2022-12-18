97b6d8099a42a0cd59d45c7a0109658f5b0536f3
Djany Mouton <djany.developpement@gmail.com>
2022-12-18 15:19:20 +0100

    Have to find the solution for hidde the error when input(keyup) is activated

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,20 @@
 "use strict";
 let input = document.querySelector('#email__input');
 let btn = document.querySelector('#email__btn');
+let errorText = document.querySelector('#error__div');
 btn.addEventListener('click', () => {
     if (input.value == "") {
         console.log('Error csl');
         input.placeholder = "Error";
+        input.style.border = "red 1px solid";
+        errorText.style.display = "flex";
     }
     else {
-        console.log('Great ! You will receive the email in 5 minutes');
+        // Je veux que quand j'écris après une erreur la border redeviens normal et que le span (error) disparaisse
+        input.addEventListener('keyup', () => {
+            console.log('Great ! You will receive the email in 5 minutes');
+            input.style.border = "1px rgb(212, 212, 212) solid";
+            errorText.style.display = "none";
+        });
     }
 });
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
 let input = document.querySelector('#email__input') as HTMLInputElement;
 let btn = document.querySelector('#email__btn') as HTMLButtonElement;
-
+let errorText = document.querySelector('#error__div') as HTMLSpanElement;
 btn.addEventListener('click', () => {
     if (input.value ==  "") { 
         console.log('Error csl');
         input.placeholder = "Error";
+        input.style.border = "red 1px solid"
+        errorText.style.display= "flex"
     } else {
-        console.log('Great ! You will receive the email in 5 minutes');
+        // Je veux que quand j'écris après une erreur la border redeviens normal et que le span (error) disparaisse
+        input.addEventListener('keyup', () => {
+            console.log('Great ! You will receive the email in 5 minutes');
+            input.style.border = "1px rgb(212, 212, 212) solid"
+            errorText.style.display= "none"
+        })
     }
 });
+
 
  
