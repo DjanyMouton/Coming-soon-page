0d614194e0435e920689fab8dc6eb2ce27b267a9
Djany Mouton <djany.developpement@gmail.com>
2022-12-23 19:59:34 +0100

    Try to used regex for form validation

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 let input = document.querySelector('#email__input') as HTMLInputElement;
 let btn = document.querySelector('#email__btn') as HTMLButtonElement;
 let errorText = document.querySelector('#error__div') as HTMLSpanElement;
+let emailCheckRegex = new RegExp (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g);
+
 btn.addEventListener('click', () => {
-    if (input.value ==  "") { 
+    if (input.value ==  "" || emailCheckRegex.test(input.value) == true) { 
         console.log('Error csl');
         input.style.border = "red 1px solid"
         errorText.style.display= "flex"
