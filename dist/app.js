9477935a8bcc0c157891b8011d6ab8e050804a49
Djany Mouton <djany.developpement@gmail.com>
2022-12-24 14:34:20 +0100

    Add error icon when email is invalide

--- dist/app.js
+++ dist/app.js
@@ -2,18 +2,19 @@
 let input = document.querySelector('#email__input');
 let btn = document.querySelector('#email__btn');
 let errorText = document.querySelector('#error__div');
+let errorIcon = document.querySelector('#error__icon');
 btn.addEventListener('click', () => {
     if (input.value == "") {
         console.log('Error csl');
         input.style.border = "red 1px solid";
         errorText.style.display = "flex";
+        errorIcon.style.display = "flex";
     }
     else {
-        // Je veux que quand j'écris après une erreur la border redeviens normal et que le span (error) disparaisse
-        input.addEventListener('keyup', () => {
-            console.log('Great ! You will receive the email in 5 minutes');
-            input.style.border = "1px rgb(212, 212, 212) solid";
-            errorText.style.display = "none";
-        });
+        console.log('Great ! You will receive the email in 5 minutes');
+        input.style.border = "1px rgb(212, 212, 212) solid";
+        errorText.style.display = "none";
+        errorIcon.style.display = "none";
     }
+    ;
 });
--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,20 @@
 let input = document.querySelector('#email__input') as HTMLInputElement;
 let btn = document.querySelector('#email__btn') as HTMLButtonElement;
 let errorText = document.querySelector('#error__div') as HTMLSpanElement;
-let emailCheckRegex = new RegExp (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g);
+let errorIcon = document.querySelector('#error__icon') as HTMLImageElement;
 
 btn.addEventListener('click', () => {
-    if (input.value ==  "" || emailCheckRegex.test(input.value) == true) { 
+    if (input.value ==  "") { 
         console.log('Error csl');
-        input.style.border = "red 1px solid"
-        errorText.style.display= "flex"
+        input.style.border = "red 1px solid";
+        errorText.style.display = "flex";
+        errorIcon.style.display = "flex";
     } else {
-        // Je veux que quand j'écris après une erreur la border redeviens normal et que le span (error) disparaisse
-        input.addEventListener('keyup', () => {
             console.log('Great ! You will receive the email in 5 minutes');
-            input.style.border = "1px rgb(212, 212, 212) solid"
-            errorText.style.display= "none"
-        })
-    }
+            input.style.border = "1px rgb(212, 212, 212) solid";
+            errorText.style.display= "none";
+            errorIcon.style.display= "none";
+    };
 }); 
 
 
