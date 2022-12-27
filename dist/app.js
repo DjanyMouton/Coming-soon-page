5f2d8e0ace978c33bca3bf70a1fde9f93e553b24
Djany Mouton <djany.developpement@gmail.com>
2022-12-27 20:04:18 +0100

    Email verification with successful regex

--- dist/app.js
+++ dist/app.js
@@ -1,20 +1,27 @@
 "use strict";
-let input = document.querySelector('#email__input');
-let btn = document.querySelector('#email__btn');
-let errorText = document.querySelector('#error__div');
-let errorIcon = document.querySelector('#error__icon');
-btn.addEventListener('click', () => {
-    if (input.value == "") {
-        console.log('Error csl');
-        input.style.border = "red 1px solid";
-        errorText.style.display = "flex";
-        errorIcon.style.display = "flex";
-    }
-    else {
-        console.log('Great ! You will receive the email in 5 minutes');
-        input.style.border = "1px rgb(212, 212, 212) solid";
-        errorText.style.display = "none";
-        errorIcon.style.display = "none";
-    }
-    ;
-});
+const input = document.querySelector('#email__input');
+const btn = document.querySelector('#email__btn');
+const errorText = document.querySelector('#error__div');
+const errorIcon = document.querySelector('#error__icon');
+const form = document.querySelector('form');
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+function showError() {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault;
+        if (input.value.match(emailRegex)) {
+            console.log('Reussi !');
+            input.style.border = "1px rgb(212, 212, 212) solid";
+            errorText.style.display = "none";
+            errorIcon.style.display = "none";
+            input.value = "";
+        }
+        else {
+            console.log('Erreur !');
+            input.style.border = "red 1px solid";
+            errorText.style.display = "flex";
+            errorIcon.style.display = "flex";
+            input.value = "";
+        }
+    });
+}
+showError();
--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,27 @@
-let input = document.querySelector('#email__input') as HTMLInputElement;
-let btn = document.querySelector('#email__btn') as HTMLButtonElement;
-let errorText = document.querySelector('#error__div') as HTMLSpanElement;
-let errorIcon = document.querySelector('#error__icon') as HTMLImageElement;
+const input = document.querySelector('#email__input') as HTMLInputElement;
+const btn = document.querySelector('#email__btn') as HTMLButtonElement;
+const errorText = document.querySelector('#error__div') as HTMLSpanElement;
+const errorIcon = document.querySelector('#error__icon') as HTMLImageElement;
+const form = document.querySelector('form') as HTMLFormElement;
+const emailRegex : RegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-btn.addEventListener('click', () => {
-    if (input.value ==  "") { 
-        console.log('Error csl');
-        input.style.border = "red 1px solid";
-        errorText.style.display = "flex";
-        errorIcon.style.display = "flex";
-    } else {
-            console.log('Great ! You will receive the email in 5 minutes');
+function showError() {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault;
+        if(input.value.match(emailRegex)) {
+            console.log('Reussi !');
             input.style.border = "1px rgb(212, 212, 212) solid";
             errorText.style.display= "none";
             errorIcon.style.display= "none";
-    };
-}); 
+            input.value = "";
+        } else {
+            console.log('Erreur !');
+            input.style.border = "red 1px solid";
+            errorText.style.display = "flex";
+            errorIcon.style.display = "flex";
+            input.value = "";
+        }
+    })
+}
 
-
- 
+showError();
